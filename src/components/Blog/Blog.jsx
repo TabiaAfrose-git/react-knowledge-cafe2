be01943841a5c8b22ed32d2320513970cd87e012
TabiaAfrose-git <tabiaafrose16@gmail.com>
2024-09-02 18:27:54 +0600

    add too any props in Blog Component

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,32 @@
 import PropTypes from 'prop-types';
+import './Blog.css'
 
 const Blog = ({blog}) => {
-    const {title,cover,author} = blog;
+    const {title,cover,author,author_img,posted_date,reading_time,hashtags} = blog;
     return (
-        <div>
-            <img className='p-4' src={cover} alt={`cover the picture of the title ${title}`} />
-            <h2 className="text-4xl p-6">{title}</h2>
-            <p>Author: {author}</p>
+        <div className='m-2 border-b-2 '>
+            <div className='blog-background p-2 rounded-md'>
+                <img className='mb-4' src={cover} alt={`cover the picture of the title ${title}`} />
+                <div className='flex justify-between'>
+                    <div className='flex gap-2'>
+                        <img className='w-16'src={author_img} alt="" />
+                        <div>
+                            <h3>{author}</h3>
+                            <p className='text-sm'>{posted_date}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <small className='text-sm'> {reading_time} min read</small>
+                    </div>
+                </div>
+                <h2 className="text-4xl">{title}</h2>
+                <p className='text-sm'>
+                    {
+                        hashtags.map((hash,idx) => <span key = {idx}><a>{hash}</a></span>)
+                    }
+                </p>
+                <a className='text-sm' href="">Mark as read</a>
+            </div>
         </div>
     );
 };
